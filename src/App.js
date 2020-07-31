cedb3259f15edc0bb03f12b5724d7a4717e51f73
sokthearymao <68365381+sokthearymao@users.noreply.github.com>
2020-07-31 17:10:13 -0600

    Update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import './App.css'
 import ClassCounter from './components/ClassCounter'
 import HookCounter from './components/HookCounter'
@@ -7,17 +7,79 @@ import HookCounterThree from './components/HookCounterThree'
 import HookCounterFour from './components/HookCounterFour'
 import ClassCounterOne from './components/ClassCounterOne'
 import HookCounterOne from './components/HookCounterOne'
+import HookMouse from './components/HookMouse'
+import ClassMouse from './components/ClassMouse'
+import MouseContainer from './components/MouseContainer'
+import IntervalClassCounter from './components/IntervalClassCounter'
+import IntervalHookCounter from './components/IntervalHookCounter'
+import DataFetching from './components/DataFetching'
+import CounterOne from './components/CounterOne'
+import CounterTwo from './components/CounterTwo'
+import CounterThree from './components/CounterThree'
+import ComponentA from './components/ComponentA'
+import ComponentB from './components/ComponentB'
+import ComponentC from './components/ComponentC'
+import DataFetchingOne from './components/DataFetchingOne'
+import DataFetchingTwo from './components/DataFetchingTwo'
+import ParentComponent from './components/ParentComponent'
+
+export const UserContext = React.createContext()
+export const ChannelContext = React.createContext()
+
+export const CountContext = React.createContext()
+
+
+// const initialState = 0
+// const reducer = (state, action) => {
+//     switch(action) {
+//         case 'increment':
+//             return state + 1
+//         case 'decrement':
+//             return state -1
+//         case 'reset':
+//             return initialState
+//         default:
+//             return state
+//     }
+// }
 
 function App() {
+  // const [count, dispatch] = useReducer(reducer, initialState)
   return (
     <div className="App">
-      {/* <ClassCounter /> */}
-      {/* <HookCounter /> */}
-      {/* <HookCounterTwo /> */}
-      {/* <HookCounterThree /> */}
-      {/* <HookCounterFour /> */}
-      <ClassCounterOne />
-      {/* <HookCounterOne /> */}
+        {/* <CountContext.Provider 
+          value={{ countState: count, countDispatch: dispatch }}
+        >*/}
+        {/* <ClassCounter /> */}
+        {/* <HookCounter /> */}
+        {/* <HookCounterTwo /> */}
+        {/* <HookCounterThree /> */}
+        {/* <HookCounterFour /> */}
+        {/* <ClassCounterOne /> */}
+        {/* <HookCounterOne /> */}
+        {/* <ClassMouse /> */}
+        {/* <HookMouse /> */}
+        {/* <MouseContainer /> */}
+        {/* <IntervalClassCounter />
+        <IntervalHookCounter /> */}
+        {/* <DataFetching /> */}
+        {/* <UserContext.Provider value={'Terry'}>
+          <ChannelContext.Provider value={'Codevolution'}>
+            <ComponentC />
+          </ChannelContext.Provider>
+        </UserContext.Provider> */}
+        {/* <CounterOne /> */}
+        {/* <CounterTwo /> */}
+        {/* <CounterThree /> */}
+        {/* Count - {count}
+        <ComponentA />
+        <ComponentB />
+        <ComponentC />
+        </CountContext.Provider> */}
+        {/* <CounterThree /> */}
+        {/* <DataFetchingOne /> */}
+        {/* <DataFetchingTwo /> */}
+        <ParentComponent />
     </div>
   )
 }
---
+++ src/components/IntervalHookCounter.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react'
+
+function IntervalHookCounter() {
+    const [count, setCount] = useState(0)
+
+    const tick = () => {
+        setCount(prevCount => prevCount + 1)
+    }
+
+    function doSomething() {
+        console.log()
+    }
+
+
+    useEffect(() => {
+        doSomething()
+        const interval = setInterval(tick, 1000)
+        return () => {
+            clearInterval(interval)
+        }
+    }, [])
+
+    return (
+        <div>
+            {count}
+        </div>
+    )
+}
+
+export default IntervalHookCounter
